7f818622d39d693cdee07a094a261df601d2ac50
Jacob Child <98500878+JacobChild@users.noreply.github.com>
2022-01-26 20:30:52 -0700

    more understanding

--- local_branching_description.jl
+++ local_branching_description.jl
@@ -3,4 +3,6 @@
     It looks like when I create a new branch (test_branch) in Atom, it
     changes my local files to just the branch files, not the main files.
     This means I "lose" the main files locally, but they are all online?
+    Update: whichever branch I hvae selected in Atom, is the one who's files
+        I can see and edit. I think I do have to hit fetch though
 =#
