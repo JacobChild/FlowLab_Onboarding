c18397d72d3f8e09382e42446e658ebd970c954e
Jacob Child <98500878+JacobChild@users.noreply.github.com>
2022-01-26 20:14:32 -0700

    I learned how to comment, also indentations, not brackets control format

--- testfile.jl
+++ testfile.jl
@@ -1,2 +1,13 @@
-i don't know how to comment yet
-I still don't know how to comment, but do I know how to commit?
+#testfile.jl
+#= Jacob Child
+    Jan 26, 2022
+        I learned how to comment and commit!!!
+        I will be using test code from Medium
+=#
+
+function sphere_vol(r) #near as i can tell this creates a function with input r
+    return 4/3*pi*r^3
+end
+
+print("volume: ", sphere_vol(5)) #looks like you can print and call all in one line. Go Julia!
+#it ran! It also outputs 13 decimal places! answer= 523.599 fyi
