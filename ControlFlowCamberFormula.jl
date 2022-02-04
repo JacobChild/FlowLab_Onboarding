0804e7cf66e6af7c3a833e76b2ad2d54d65f406d
Jacob Child <98500878+JacobChild@users.noreply.github.com>
2022-02-03 20:12:03 -0700

    Start of Activity 2

---
+++ ControlFlowCamberFormula.jl
@@ -0,0 +1,9 @@
+#=
+ControlFlowCamberFormula.jl
+Jacob Child
+Flow Lab Onboarding
+Feb 3, 2022
+Background: A Camber line is "a measure of the curvature of an airfoil"
+    It is halfway between the upper and lower curves of the airfoil
+
+=#
