afc5471f0fa5a112125789c9a4b12362a5bbef53
Jacob Child <98500878+JacobChild@users.noreply.github.com>
2022-04-24 01:40:01 -0600

    hmm, not much headway

--- LeapFrogger.jl
+++ LeapFrogger.jl
@@ -3,4 +3,20 @@ LeapFrogger.jl
 Jacob Child
 Flow Lab Onboarding Project
 April 23rd, 2022
+
+Equations
+V(vector) = (Gamma(vector) cross r(vector))/(2*pi*r^2)
+
 =#
+
+#test stuffs
+Gamma = [0 0 1]
+d = 1
+t = [0:.01:.05;]
+transpose(t)
+r =  1   #not sure what the r vector is?
+
+V = ((Gamma.^t)*r)/(2*pi*d^2/4)
+#println(t)
+println(V)
+
